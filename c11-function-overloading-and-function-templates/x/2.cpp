ff2fb7faaadf3e20c4957f286c215fda9fbfd17b
Konstantin Terziev <terzievk@yahoo.com>
2026-06-16 22:29:35 +0300

    11.x

---
+++ c11-function-overloading-and-function-templates/x/2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+// write your add function template here
+template <typename T>
+T add(T x, T y) {
+  return x + y;
+}
+
+template <typename T>
+T mult(T x, int y) {
+  return x * y;
+}
+
+template <typename T, typename N>
+auto sub(T x, N y) {
+  return x - y;
+}
+
+int main() {
+  // part 1
+  std::cout << add(2, 3) << '\n';
+  std::cout << add(1.2, 3.4) << '\n';
+
+  // part 2
+  std::cout << mult(2, 3) << '\n';
+  std::cout << mult(1.2, 3) << '\n';
+
+  // part 3
+  std::cout << sub(3, 2) << '\n';
+  std::cout << sub(3.5, 2) << '\n';
+  std::cout << sub(4, 1.5) << '\n';
+  return 0;
+}
