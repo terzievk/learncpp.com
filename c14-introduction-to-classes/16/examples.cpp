e745e32bfe5fcbc5fc1c02a7f0d67b96f39f471e
Konstantin Terziev <terzievk@yahoo.com>
2026-07-03 19:21:32 +0300

    14.16

---
+++ c14-introduction-to-classes/16/examples.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class Dollars
+{
+private:
+  int m_dollars{};
+
+public:
+  explicit Dollars(int d) // now explicit
+  : m_dollars{ d }
+  {
+  }
+
+  int getDollars() const { return m_dollars; }
+};
+
+void print(Dollars d)
+{
+  std::cout << "$" << d.getDollars();
+}
+
+int main()
+{
+  print(Dollars {5}); // compilation error because Dollars(int) is explicit
+
+
+  return 0;
+}
