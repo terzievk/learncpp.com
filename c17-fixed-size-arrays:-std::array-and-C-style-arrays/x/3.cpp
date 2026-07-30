11b6e70dddf6aef41191a878e09df25df86735dc
Konstantin Terziev <terzievk@yahoo.com>
2026-07-30 16:32:10 +0300

    3.cpp step 1

---
+++ c17-fixed-size-arrays:-std::array-and-C-style-arrays/x/3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+enum ranks {
+  ace,
+  two,
+  three,
+  four,
+  five,
+  six,
+  seven,
+  eight,
+  nine,
+  ten,
+  jack,
+  queen,
+  king,
+  ranks_count
+};
+
+enum suits {
+  clubs,
+  diamonds,
+  hearts,
+  spades,
+  suits_count
+};
+
+int main() {
+  std::cout << "Hiya!\n";
+}
